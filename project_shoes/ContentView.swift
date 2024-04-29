22c1cfd455fe6341123fbd0be6a9b7b4ce497407
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-29 14:00:11 -0300

    first commit

--- project_shoes/ContentView.swift
+++ project_shoes/ContentView.swift
@@ -1,21 +1,18 @@
+
 //
 //  ContentView.swift
-//  project_shoes
+//  shoes_app
 //
-//  Created by Marcelo Amaral Alves on 2024-04-29.
-//
+//  Created by Marcelo Amaral Alves
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ScrollView(.vertical, showsIndicators: false, content: {
+            Home()
+        })
+        .preferredColorScheme(.light)
     }
 }
 
