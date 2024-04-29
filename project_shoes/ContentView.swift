88cb2da15b3bf6e1675efa1b9873a1e7ba238333
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-29 13:59:25 -0300

    Initial Commit

---
+++ project_shoes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project_shoes
+//
+//  Created by Marcelo Amaral Alves on 2024-04-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project_shoes/project_shoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_shoesApp.swift
+//  project_shoes
+//
+//  Created by Marcelo Amaral Alves on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct project_shoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
